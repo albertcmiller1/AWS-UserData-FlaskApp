66cbe988bd64c08162a7f37bcca549d3cc9e97b6
Albert Miller <albertcmiller1@gmail.com>
2021-07-26 12:52:58 -0500

    not much new

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
-from flask import Flask 
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 print("app is running")
 
 @app.route("/")
-
 def hello_world():
-    return "<p>Hello, World</p>"
+    # return render_template('./index.html')
+    return jsonify(first="albert", age=24)
 
 
-app.run(host='0.0.0.0', debug=True, port=5000)
+@app.route("/hey")
+def print_hey():
+    # return render_template('./index.html')
+    return jsonify(first="there", second="delila")
+
+
+# app.run(host='0.0.0.0', debug=True, port=5000)
+
+if __name__ == "__main__":
+    app.run()
+
+    
